cc1a45170997e745e222cdabb7bc6dbe2e5f8f3f
Dino Tudor <dinotudor@gmail.com>
2019-08-20 01:14:37 +0200

    get all projects route

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ### Node.JS
 
+_CRUD_ operations
+
 #### ROUTES
 
-- POST "/projects" - { id: "1", title: "New Project", tasks: [] };
+- POST "/projects" - create new project with unique Id in this format: { id: "1", title: "New Project", tasks: [] };
 - GET "/projects" - list all projects and all tasks;
 - PUT "/projects/:id" - edit **Title** in project with given Id;
 - DELETE "/projects/:id" - delete project with given Id;
--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-'use-strict'
+'use-strict';
 
 const express = require('express');
 
 const app = express();
 
+const projects = [
+  {
+    id: '1',
+    title: 'Test',
+    tasks: []
+  },
+  {
+    id: '2',
+    title: 'More tests',
+    tasks: []
+  },
+  {
+    id: '3',
+    title: 'Even more tests',
+    tasks: []
+  }
+];
+
+app.get('/projects', (req, res) => {
+  res.json(projects);
+});
+
 app.listen(3100);
+
+/*
+- POST "/projects" - { id: "1", title: "New Project", tasks: [] };
+- GET "/projects" - list all projects and all tasks;
+- PUT "/projects/:id" - edit **Title** in project with given Id;
+- DELETE "/projects/:id" - delete project with given Id;
+- POST "/projects/:id/tasks" - given **title** and **id** of a project, store a new task inside tasks array
+*/
